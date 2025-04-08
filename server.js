d101876fc18fb928d3ba11ac6251dc4ecc69cd19
tarin-james <tarinwardwell@gmail.com>
2025-04-07 21:16:50 -0600

    redirect to frontend

--- server.js
+++ server.js
@@ -1,79 +1,90 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./data/database');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("./data/database");
 
 const app = express();
 const port = process.env.PORT || 8080;
 mongodb.initDb((err) => {
-    if(err) {
-        console.log(err)
-    }
-    else {
-        app.listen(port, () => {console.log(`Database is listening and node Running on port ${port}`)});
-    }
-})
-const passport = require('passport');
-const session = require('express-session');
-const GitHubStrategy = require('passport-github2').Strategy;
-const cors = require('cors');
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Database is listening and node Running on port ${port}`);
+    });
+  }
+});
+const passport = require("passport");
+const session = require("express-session");
+const GitHubStrategy = require("passport-github2").Strategy;
+const cors = require("cors");
 
-
-
-
-
- app
-    .use(bodyParser.json())
-    .use(session({
-        secret: "secret",
-        resave: false,
-        saveUninitialized: true,
-    }))
-    .use(passport.initialize())
-    .use(passport.session())
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader(
-               'Access-Control-Allow-Headers',
-                'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-            );
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-                 next();
+app
+  .use(bodyParser.json())
+  .use(
+    session({
+      secret: "secret",
+      resave: false,
+      saveUninitialized: true,
     })
-    .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}))
-    .use(cors({ origin: '*'}))
-    .use("/", require("./routes/index.js"));
+  )
+  .use(passport.initialize())
+  .use(passport.session())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    next();
+  })
+  .use(cors({ methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"] }))
+  .use(cors({ origin: "*" }))
+  .use("/", require("./routes/index.js"));
 
-
-    passport.use (new GitHubStrategy({
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: process.env.CALLBACK_URL
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
     },
-    function(accessToken, refreshToken, profile, done) {
-        //User.findOrCreate({ githubId: profile.id }, function (err, user){
-            return done(null, profile);
-        //})
+    function (accessToken, refreshToken, profile, done) {
+      //User.findOrCreate({ githubId: profile.id }, function (err, user){
+      return done(null, profile);
+      //})
     }
-    ));
+  )
+);
 
-    passport.serializeUser((user, done) => {
-        done(null, user);
-    });
-    passport.deserializeUser((user, done) =>{
-        done(null, user);
-    });
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
-    app.get('/', (req, res) => {
-        res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")
-    });
+app.get("/", (req, res) => {
+  res.send(
+    req.session.user !== undefined
+      ? `Logged in as ${req.session.user.displayName}`
+      : "Logged Out"
+  );
+});
 
-    app.get('/github/callback', passport.authenticate('github', {
-        failureRedirect: '/api-docs', session: false}),
-    (req,res) => {
-        req.session.user = req.user;
-        res.redirect('/');
-    });
-
-
+app.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/api-docs",
+    session: false,
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect("https://sports-stop-frontend.onrender.com/");
+  }
+);
 
 module.exports = app;
