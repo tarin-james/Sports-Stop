2e8f31b02a83efd7ab874adcca394eb74e66dca8
tarin-james <tarinwardwell@gmail.com>
2025-04-09 23:18:30 -0600

    Change order

--- server.js
+++ server.js
@@ -38,16 +38,16 @@ app
   .use(passport.session())
   .use((req, res, next) => {
     res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.setHeader(
       "Access-Control-Allow-Origin",
       "https://sports-stop-frontend.onrender.com"
     );
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS"
     );
     res.setHeader("Access-Control-Allow-Credentials", "true");
     next();
